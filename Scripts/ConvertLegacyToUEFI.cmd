2185fa49a4f5a833baad01884cb9801ea3fb2c9a
nelsonbrett55 <61249837+nelsonbrett55@users.noreply.github.com>
2025-10-15 10:11:27 -0700

    Update ConvertLegacyToUEFI.cmd

--- Scripts/ConvertLegacyToUEFI.cmd
+++ Scripts/ConvertLegacyToUEFI.cmd
@@ -37,7 +37,7 @@ echo.
 :: +---------------------------------------------------+
 :: ! Check if BIOS is already UEFI                     !
 :: +---------------------------------------------------+
-    for /f "delims=" %%A in ('powershell -command "$env:firmware_type"') do set firmware=%%A
+    set "firmware=%firmware_type%" 
     if /I "%firmware%"=="UEFI" (
        echo Operating system is already running in UEFI mode.
        goto :EOF
@@ -48,13 +48,12 @@ echo.
 :: +---------------------------------------------------+
 :: ! Check Partition Count                             !
 :: +---------------------------------------------------+
-    for /f "skip=1 tokens=2 delims==" %%A in ('wmic diskdrive get partitions /value ^| find "="') do set partitions=%%A
+    for /f "skip=1 tokens=2 delims==" %%A in ('wmic diskdrive get partitions /value') do set partitions=%%A
     if "%partitions%"=="" set partitions=0
-    if %partitions% GTR 3 (
-        echo Too many partitions detected (%partitions%). Conversion not recommended.
-        echo Conversion requires 3 or fewer partitions.
-        goto :EOF
-    )
+    if %partitions% GTR 3 echo Too many partitions detected (%partitions%). Conversion not recommended.
+    if %partitions% GTR 3 echo Conversion requires 3 or fewer partitions.
+    if %partitions% GTR 3 goto :EOF
+    
     echo Partition Count: %partitions%
     echo.
 
@@ -62,10 +61,10 @@ echo.
 :: ! Validate MBR2GPT Conversion                       !
 :: +---------------------------------------------------+
     mbr2gpt /validate /allowfullos | find /i "MBR2GPT: Validation completed successfully" >nul
-    if errorlevel 1 (
-        echo MBR2GPT failed to validate for UEFI. Conversion cannot continue.
-        goto :EOF
-    )
+    if errorlevel 1 Set "MBR2GPTFailed=1"
+	if "%MBR2GPTFailed%" NEQ "" echo MBR2GPT failed to validate for UEFI. Conversion cannot continue.
+    if "%MBR2GPTFailed%" NEQ "" goto :EOF
+    
     echo Validation successful.
     echo.
 
@@ -74,14 +73,13 @@ echo.
 :: +---------------------------------------------------+
     for /f "tokens=2 delims=:" %%A in ('reagentc /info ^| find /i "Windows RE status"') do set restatus=%%A
     set restatus=%restatus: =%
-    if /I "%restatus%"=="Disabled" (
-        echo Windows Recovery Environment is disabled.
-        echo It is recommended to enable before conversion.
-        echo To enable: reagentc /enable
-        echo.
-    ) else (
-        echo Windows RE status: %restatus%
-    )
+    if /I "%restatus%"=="Disabled" echo Windows Recovery Environment is disabled.
+    if /I "%restatus%"=="Disabled" echo It is recommended to enable before conversion.
+    if /I "%restatus%"=="Disabled" echo To enable: reagentc /enable
+    if /I "%restatus%"=="Disabled" echo.
+    if /I "%restatus%"=="Disabled" goto :EOF
+    
+	echo Windows RE status: %restatus%
     echo.
 
 :: +---------------------------------------------------+
@@ -98,11 +96,12 @@ echo.
 :: +---------------------------------------------------+
 :: ! Verify Conversion Result                          !
 :: +---------------------------------------------------+
-    wmic diskdrive get PartitionStyle | find /i "GPT" >nul
-    if errorlevel 1 (
-        echo Disk is still MBR. Conversion did not succeed.
-        goto :EOF
-    )
+	for /f "delims=" %%A in ('powershell -command "Get-Disk"') do set GetDisk=%%A
+	echo %GetDisk% | Find "GPT"
+    if errorlevel 1 Set "DiskMBR=1"
+    if "%DiskMBR%" NEQ "" echo Disk is still MBR. Conversion did not succeed.
+    if "%DiskMBR%" NEQ "" goto :EOF
+    
     echo Disk partition style is now GPT.
     echo.
 
